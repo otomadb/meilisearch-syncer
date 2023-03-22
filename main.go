40dde996827fba27775dd7b10831214982685bb9
SnO2WMaN <me@sno2wman.net>
2023-03-22 13:02:40 +0900

    video

--- main.go
+++ main.go
@@ -19,14 +19,21 @@ func main() {
 
 	ms := meilisearch.NewClient(meilisearch.ClientConfig{Host: os.Getenv("MEILISEARCH_URL")})
 
+	update_tags(conn, ms)
+	update_videos(conn, ms)
+
+	fmt.Println("Synced successfully.")
+}
+
+func update_tags(conn *pgx.Conn, ms *meilisearch.Client) {
 	rows, err := conn.Query(context.Background(), `SELECT "id","name","tagId" FROM "TagName"`)
+	defer rows.Close()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	var tagDocuments []map[string]interface{}
-	defer rows.Close()
+	var docs []map[string]interface{}
 	for rows.Next() {
 		var id string
 		var name string
@@ -38,28 +45,86 @@ func main() {
 			os.Exit(1)
 		}
 
-		tagDocuments = append(tagDocuments, map[string]interface{}{"id": id, "name": name, "tag_id": tagId})
+		docs = append(docs, map[string]interface{}{"id": id, "name": name, "tag_id": tagId})
 	}
 
-	tags_index := ms.Index("tags")
+	index := ms.Index("tags")
 
-	_, err = tags_index.DeleteAllDocuments()
+	_, err = index.UpdateIndex("id")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	_, err = tags_index.UpdateDistinctAttribute("tag_id")
+	_, err = index.UpdateDistinctAttribute("tag_id")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	_, err = tags_index.AddDocuments(tagDocuments)
+	_, err = index.DeleteAllDocuments()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Synced successfully.")
+	_, err = index.AddDocuments(docs)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Tags sync successfully.")
+}
+
+func update_videos(conn *pgx.Conn, ms *meilisearch.Client) {
+	rows, err := conn.Query(context.Background(), `SELECT "id","title","videoId" FROM "VideoTitle"`)
+	defer rows.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	var docs []map[string]interface{}
+	for rows.Next() {
+		var id string
+		var title string
+		var videoId string
+
+		err = rows.Scan(&id, &title, &videoId)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to scan the row: %v\n", err)
+			os.Exit(1)
+		}
+
+		docs = append(docs, map[string]interface{}{"id": id, "title": title, "video_id": videoId})
+	}
+
+	index := ms.Index("videos")
+
+	_, err = index.UpdateIndex("id")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	_, err = index.UpdateDistinctAttribute("video_id")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	_, err = index.DeleteAllDocuments()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	_, err = index.AddDocuments(docs)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Videos sync successfully.")
 }
